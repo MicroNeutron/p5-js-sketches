bfa73dcec6274487665d6f1bae9904cb2192c6a1
MicroNeutron <90130404+MicroNeutron@users.noreply.github.com>
2024-06-04 13:49:49 -0400

    Update sketch3.js

--- sketches/sketch3.js
+++ sketches/sketch3.js
@@ -5,16 +5,17 @@ function setup() {
 function draw() {
   background(200); 
   
-  if(keyIsDown(65)){ // When the user presses the 'A' key
+  if(keyIsDown(65)) { // When the user presses the 'A' key
     fill(255, 0, 255); // in Purple color
     triangle(100, 100, 150, 50, 200, 100); //draw a triangle with vertices at (100, 100), (150, 50), and (200,100)
   }
-  if(keyIsDown(66)){ // When the user presses the 'B' key
-    fill(255, 102, 0); // in Orange color.
+  if(keyIsDown(66)) { // When the user presses the 'B' key
+    fill(255, 165, 0); // in Orange color.
     triangle(250, 250, 300, 200, 350, 250); // it will draw a triangle with vertices at (250, 250), (300, 200), and (350,250)
   }
-  if(mouseIsPressed){
-  
+  if(mouseIsPressed) { // When the user clicks the mouse
+    fill(255);
+    troangle(mouseX, mouseY + 57.3, mouseX - 50, mouseY - 28.9, mouseX + 50， mouseY - 28.9); //draw a triangle centered at the mouse position with verticesforming an equilateral triangle of side length 100
   }
 }
 
