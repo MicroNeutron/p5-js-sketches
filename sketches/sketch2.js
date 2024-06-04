d5a33499bd1c51e32f238c678edd6839b380ae9e
MicroNeutron <90130404+MicroNeutron@users.noreply.github.com>
2024-06-04 13:10:50 -0400

    Update sketch2.js

---
+++ sketches/sketch2.js
@@ -0,0 +1,20 @@
+function setup(){
+    createCanvas(400, 400);
+    background(200);
+}
+
+function draw(){
+    if (keyIsPressed){ // When the user presses any key
+        fill(255, 255, 0); // in Yellow color
+        ellipse(width/2, height/2, 150, 100); // draw an ellipse at the center of the canvas with a width of 150 andheight of 100
+    }
+}
+
+function keyReleased(){ // When the user releases any key
+    noFill(); 
+    ellipse(mouseX, mouseY, 50, 75); // draw an ellipse at the mouse position with a width of 50 andheight of 75
+}
+
+function doubleClicked(){ // When the user double-clicks the mouse
+    background(200); // clear the canvas
+}
