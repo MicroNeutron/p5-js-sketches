db668f0f211f22a726a9f9f18d971ba8cb176e33
MicroNeutron <90130404+MicroNeutron@users.noreply.github.com>
2024-06-04 14:24:16 -0400

    Update sketch6.js

--- sketches/sketch6.js
+++ sketches/sketch6.js
@@ -6,4 +6,20 @@ function setup() {
   background(2);
 }
 
+function draw() {
+  if (mouseButton === LEFT) { // When the user presses the mouse
+    fill(random(colorlist)); // Use a different color for each circle
+    circle(mouseX, mouseY, 10); // draw a series of concentric circles centered at the mouse position
+    circle(mouseX, mouseY, 20); // draw a series of concentric circles centered at the mouse position
+    circle(mouseX, mouseY, 30); // draw a series of concentric circles centered at the mouse position
+    circle(mouseX, mouseY, 40); // draw a series of concentric circles centered at the mouse position
+    circle(mouseX, mouseY, 50); // draw a series of concentric circles centered at the mouse position
+  }
+
+  for (let n = 0; n < 5; n++) { // Use a for loop
+    fill(random(colorlist)); // Use a different color for each circle
+    circle(mouseX, mouseY, n * 10); // to draw 5 concentric circles with increasing radii
+  }
+}
+
 
