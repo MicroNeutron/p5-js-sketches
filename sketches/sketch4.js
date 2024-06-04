1c755f64a54442281e35409bbe43a8246ff14e83
MicroNeutron <90130404+MicroNeutron@users.noreply.github.com>
2024-06-04 13:56:28 -0400

    Update sketch4.js

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,16 +1,16 @@
 function setup(){
   createCanvas(400, 400); // 
-  background(200); 
+  background(2); 
 }
 
 function draw(){
-  if (mouseIsPressed){ // when mouse is pressed
-    stroke(255, 0, 0); // red
-    line(0, 0, width, height); // top left to bottom right
+  if (mouseIsPressed){ // When the user presses the mouse
+    stroke(255, 0, 0); // Use different colors for the two lines
+    line(0, 0, width, height); // draw a line from the top-left corner to the bottom-right cornerof the canvas
   }
 }
 
-function mouseReleased(){ // when mouse is released
-  stroke(0, 0, 255); // blue
-  line(width, 0, 0, height); // top right to bottom left
+function mouseReleased(){ // When the user releases the mouse
+  stroke(0, 0, 255); // Use different colors for the two lines
+  line(width, 0, 0, height); // draw a line from the top-right corner to the bottom-left cornerof the canvas
 }
