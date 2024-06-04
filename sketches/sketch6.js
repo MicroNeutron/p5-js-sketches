ec8ec34be9b10e0cea89747abbed55de8c82e94e
MicroNeutron <90130404+MicroNeutron@users.noreply.github.com>
2024-06-04 14:15:18 -0400

    Update sketch6.js

--- sketches/sketch6.js
+++ sketches/sketch6.js
@@ -1,1 +1,9 @@
+let colorlist = ['gold', 'yellow', 'turquoise', 'red', 'blue', 'green', 'purple', 'pink', 'orange', 'white'];
+
+function setup() {
+  //canvas
+  createCanvas(windowWidth, windowHeight);
+  background(2);
+}
+
 
