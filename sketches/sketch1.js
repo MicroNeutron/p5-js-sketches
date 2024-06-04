02b66fab55323f8618161748d941e61ff9a7d23a
MicroNeutron <90130404+MicroNeutron@users.noreply.github.com>
2024-06-04 13:15:19 -0400

    Update sketch1.js

--- sketches/sketch1.js
+++ sketches/sketch1.js
@@ -5,16 +5,16 @@ function setup() {
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW){
-    fill(0, 0, 255);
-    circle(100, 100, 50);
+  if (keyCode === LEFT_ARROW){ // When the user presses the left arrow
+    fill(0, 0, 255); // in Blue color
+    circle(100, 100, 50); // draw a circle at (100, 100) with a diameter of 50
   }   
-  if (keyCode === LEFT_ARROW) {
-    fill(0, 255, 0);
-    circle(200, 200, 50);
+  if (keyCode === LEFT_ARROW) { // When the user presses the right arrow
+    fill(0, 255, 0); // in Greencolor
+    circle(200, 200, 50); // draw a circle at (200, 200) with a diameter of 50
   }
-  if (keyCode === 32) {  
-    fill(255);
-    circle(mouseX, mouseY, 75);
+  if (keyCode === 32) { // When the user presses the spacebar
+    fill(255); 
+    circle(mouseX, mouseY, 75); // draw a circle at the mouse position with a diameter of 75.
   }
 }
